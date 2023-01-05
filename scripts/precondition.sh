0cf075509f90bd5b0798c03d316a72ff412030f0
Krijn Doekemeijer <krijn.doekemeijer@gmail.com>
2023-01-05 08:52:12 +0100

    Update precondition scripts

--- scripts/precondition.sh
+++ scripts/precondition.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-precondition_fill_nvme() {
+trim_nvme() {
     dev=$1;
     sudo nvme format -s 1 $dev;
+}
+
+precondition_fill_nvme() {
+    dev=$1;
+    trim_nvme $dev;
     sudo $fio \
         --name "precondition_fill_nvme" \
         --filename=$dev \
@@ -18,6 +23,7 @@ precondition_fill_nvme() {
 precondition_rand_nvme() {
     dev=$1;
     size_prec=$2;
+    mkdir -p data/steady_state;
     sudo $fio \
         --name "precondition_rand_nvme" \
         --filename=$dev \
@@ -29,6 +35,7 @@ precondition_rand_nvme() {
         --thread=1 \
         --ioengine=io_uring \
         --fixedbufs=1 --registerfiles=1 --hipri;
+        --write_bw_log=data/steady_state/unsteady_state_${size_prec};
 }
 
 steady_state_nvme() {
@@ -37,7 +44,7 @@ steady_state_nvme() {
     steady=$3;
     mkdir -p data/steady_state;
     sudo $fio \
-        --name "precondition_2" \
+        --name "steady_state_nvme" \
         --filename=$dev \
         --runtime=30m \
         --ss=iops:$steady \
@@ -51,3 +58,4 @@ steady_state_nvme() {
         --fixedbufs=1 --registerfiles=1 --hipri \
         --write_bw_log=data/steady_state/steady_state_${size_prec};
 }
+
